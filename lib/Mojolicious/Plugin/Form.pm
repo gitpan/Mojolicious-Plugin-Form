bedb891acef7093c718bcaead1f32b58dda27b4f
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2014-08-16 07:21:02 +0000

    Import of WOLLMERS/Mojolicious-Plugin-Form-0.004 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-Form
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         WOLLMERS/Mojolicious-Plugin-Form-0.004.tar.gz
    gitpan-cpan-author:       WOLLMERS
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/Form.pm
+++ lib/Mojolicious/Plugin/Form.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::Form;
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 sub register {
   my ($self, $app, $conf) = @_;
@@ -48,6 +48,10 @@ __END__
 
 Mojolicious::Plugin::Form - abstract forms for Mojolicious and DBIx::Class
 
+=for html
+
+<a href="https://travis-ci.org/wollmers/Mojolicious-Plugin-Form"><img src="https://travis-ci.org/wollmers/Mojolicious-Plugin-Form.png" alt="Mojolicious-Plugin-Form"></a>
+<a href='https://coveralls.io/r/wollmers/Mojolicious-Plugin-Form?branch=master'><img src='https://coveralls.io/repos/wollmers/Mojolicious-Plugin-Form/badge.png?branch=master' alt='Coverage Status' /></a>
 
 =head1 SYNOPSIS
 
